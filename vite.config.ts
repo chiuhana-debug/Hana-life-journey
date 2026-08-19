bafc51c893f0a6f15a462a97f4fbb57ef4c3dc09
chiuhana-debug <qiuqianheng@gmail.com>
2026-08-19 22:36:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    return {
   base: '/Hana-life-journey/',
     plugins: [react(), tailwindcss()],
     resolve: {
